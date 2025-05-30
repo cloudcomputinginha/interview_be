cef82a9046edf5fef54f098c7eeec3fced9f91ae
zjhj0814 <zjhj0814@gmail.com>
2025-05-30 19:01:48 +0900

    🐛 FIX: API마다 @Operation 작성

--- src/main/java/cloudcomputinginha/demo/web/controller/MemberInterviewRestController.java
+++ src/main/java/cloudcomputinginha/demo/web/controller/MemberInterviewRestController.java
@@ -7,6 +7,7 @@ import cloudcomputinginha.demo.service.MemberInterviewCommandService;
 import cloudcomputinginha.demo.validation.annotation.ExistInterview;
 import cloudcomputinginha.demo.web.dto.MemberInterviewRequestDTO;
 import cloudcomputinginha.demo.web.dto.MemberInterviewResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +22,14 @@ public class MemberInterviewRestController {
     private final MemberInterviewCommandService memberInterviewCommandService;
 
     @PatchMapping("/interviews/{interviewId}/waiting-room")
+    @Operation(summary = "면접 대기실 내 사용자의 상태를 변경하는 API", description = "면접, 사용자 id, 사용자 상태를 요청 받아 사용자 면접의 상태를 수정합니다.")
     public ApiResponse<MemberInterviewResponseDTO.MemberInterviewStatusDTO> changeParticipantsStatus(@PathVariable @ExistInterview Long interviewId, @RequestBody @Valid MemberInterviewRequestDTO.changeMemberStatusDTO changeMemberStatusDTO) {
         MemberInterview memberInterview = memberInterviewCommandService.changeMemberInterviewStatus(interviewId, changeMemberStatusDTO);
         return ApiResponse.onSuccess(MemberInterviewConverter.toMemberInterviewStatusDTO(memberInterview));
     }
 
     @PostMapping("/interviews/{interviewId}")
+    @Operation(summary = "면접 참여 신청 API", description = "면접, 사용자, 이력서, 자소서 id를 전부 받아와 사용자 면접을 생성합니다.")
     public ApiResponse<MemberInterviewResponseDTO.CreateMemberInterviewDTO> createMemberInterview(@PathVariable @ExistInterview Long interviewId, @RequestBody @Valid MemberInterviewRequestDTO.createMemberInterviewDTO createMemberInterviewDTO) {
         MemberInterview memberInterview = memberInterviewCommandService.createMemberInterview(interviewId, createMemberInterviewDTO);
         return ApiResponse.onSuccess(MemberInterviewConverter.toMemberInterviewResultDTO(memberInterview));
