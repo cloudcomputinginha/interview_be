6ec2b09c359150b63e01f15aebc14083ad31a52f
zjhj0814 <zjhj0814@gmail.com>
2025-06-16 22:11:01 +0900

    🐛 FIX: 비동기 설정 추가

--- src/main/java/cloudcomputinginha/demo/DemoApplication.java
+++ src/main/java/cloudcomputinginha/demo/DemoApplication.java
@@ -3,13 +3,15 @@ package cloudcomputinginha.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
