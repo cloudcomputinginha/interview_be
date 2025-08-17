6e470e3d813c7f0e830012533872ae6a6dd9d400
zjhj0814 <zjhj0814@gmail.com>
2025-08-17 19:06:37 +0900

    ♻️ Refactor: Sentry에서 에러 메시지를 찾을 수 있도록 GeneralException에서 에러 메시지 전달

--- src/main/java/cloudcomputinginha/demo/apiPayload/exception/GeneralException.java
+++ src/main/java/cloudcomputinginha/demo/apiPayload/exception/GeneralException.java
@@ -2,19 +2,22 @@ package cloudcomputinginha.demo.apiPayload.exception;
 
 import cloudcomputinginha.demo.apiPayload.code.BaseErrorCode;
 import cloudcomputinginha.demo.apiPayload.code.ErrorReasonDTO;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class GeneralException extends RuntimeException {
     private BaseErrorCode code;
+
+    public GeneralException(BaseErrorCode code) {
+        super("[" + code.getReason().getCode() + "] " + code.getReason().getMessage());  // 에러 메시지 전달
+        this.code = code;
+    }
 
     public ErrorReasonDTO getErrorReason() {
         return this.code.getReason();
     }
 
-    public ErrorReasonDTO getErrorReasonHttpStatus(){
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
         return this.code.getReasonHttpStatus();
     }
 }
