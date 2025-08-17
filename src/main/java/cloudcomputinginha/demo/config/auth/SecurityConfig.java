269352b06d4c54d3609abbfdf44928cff4ebb9d3
zjhj0814 <zjhj0814@gmail.com>
2025-08-17 21:44:43 +0900

    ♻️ Refactor: 프론트엔드 배포 도메인 변경

--- src/main/java/cloudcomputinginha/demo/config/auth/SecurityConfig.java
+++ src/main/java/cloudcomputinginha/demo/config/auth/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
                                 configuration.setAllowedOrigins(List.of(
                                         "http://localhost:8080",
                                         "http://localhost:3000",
-                                        "https://cloud-computing-fe-two.vercel.app",
                                         "http://127.0.0.1:5500",
+                                        "https://www.injob.store",
                                         domainProperties.getAi(),
                                         domainProperties.getBackend()
                                 ));
